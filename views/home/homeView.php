7a6223d6708491a4b01aedf098c9abeac35903a5
benwe95 <www.14256@ecam.be>
2017-01-08 18:38:40 +0100

    Remove some files

--- views/home/homeView.php
+++
@@ -1,3 +0,0 @@
-<h1>Welcome on Easy Travel website</h1>
-<a href="index.php?page=reservation"><input type="submit" name="action" value="New reservation"></a>
-<a href="index.php?page=manager"><input type="submit" name="action" value="Manage"></a>
